768ebd4e6cbcfeb28656e6cbaef798c50da49a6c
Schizy <ali.mechichi@gmail.com>
2024-07-22 18:17:15 +0200

    Repair template & force locale as route prefix

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,21 +5,13 @@ namespace App\Controller;
 use App\Entity\Grammar;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-
-    #[Route("/")]
-    public function indexNoLocale(): Response
-    {
-        return $this->redirectToRoute('homepage', [
-            '_locale' => $this->getParameter('kernel.default_locale'),
-        ]);
-    }
-
-    #[Route("/{_locale<%app.supported_locales%>}/", name:"homepage")]
+    #[Route("/", name:"homepage")]
     public function home(EntityManagerInterface $em): Response
     {
         return $this->render('home/home.html.twig', [
